d216c6e69353ac7ac8be236cd8a7cfe308462028
JoseLuisSR <jose.luis.sastoque.rey@gmail.com>
2026-03-06 10:21:44 -0500

    Rename folder n8n to docker.

--- aws/bootstrap.sh
+++ aws/bootstrap.sh
@@ -48,7 +48,7 @@ sudo docker compose version >/dev/null
 # (Avoid temporary 502s while Nginx is up but n8n isn't ready)
 # ------------------------------------------------------------
 log "Starting n8n stack..."
-cd /tmp/whatsapp-calendar-bot-develop/n8n
+cd /tmp/whatsapp-calendar-bot-develop/docker
 sudo docker compose up -d
 
 # ------------------------------------------------------------
